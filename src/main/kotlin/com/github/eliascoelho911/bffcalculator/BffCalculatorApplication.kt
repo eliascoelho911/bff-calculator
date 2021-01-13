427748297dc5ee82529b41d368263aef7a45e656
Elias <elias_coelho911@hotmail.com>
2021-01-12 21:10:16 -0300

    Initial commit

---
+++ src/main/kotlin/com/github/eliascoelho911/bffcalculator/BffCalculatorApplication.kt
@@ -0,0 +1,11 @@
+package com.github.eliascoelho911.bffcalculator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BffCalculatorApplication
+
+fun main(args: Array<String>) {
+	runApplication<BffCalculatorApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/eliascoelho911/bffcalculator/BffCalculatorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.eliascoelho911.bffcalculator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BffCalculatorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
